e58ea1d36078bf7d6e13ea31b8ba5c83eedad75b
johanhaha <johan.torssell@gmail.com>
2024-06-26 07:42:59 +0200

    Renamed route subsequent_org to subsequent_orgs_info

--- venture_mafia_backend/routes/routes.js
+++ venture_mafia_backend/routes/routes.js
@@ -1,11 +1,10 @@
-// routes/routes.js
 const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/controllers');
 
 router.get('/target_org/:uuid', controllers.getTargetOrgInfo);
 router.get('/alumni_info/:uuid', controllers.getAlumniInfo);
-router.get('/subsequent_orgs/:uuid', controllers.getSubsequentOrgsInfo);
+router.get('/subsequent_orgs_info/:uuid', controllers.getSubsequentOrgsInfo);
 router.get('/relations/:uuid', controllers.getRelations);
 
 module.exports = router;
